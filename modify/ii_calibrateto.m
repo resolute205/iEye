898a272e8680127623d0f755b22fdc2127880aa2
David Fegen <dfegen@gmail.com>
2014-05-01 14:14:18 -0400

    add cubic fit plot to ii_calibrate

--- modify/ii_calibrateto.m
+++ modify/ii_calibrateto.m
@@ -36,8 +36,8 @@ else
             disp('Calibration saved');
             assignin('base',chan,fit);
             ii_replot;
-            
-             %Plot
+                                     
+             % Plot
              figure
              fit_timeseries=z(1)*c1s.^3+z(2)*c1s.^2+z(3)*c1s+z(4);
              plot(fit_timeseries,'r')
@@ -47,25 +47,39 @@ else
              plot(c2s,':b')
              legend('fit','original','target')
              title([num2str(chan),'  fit to  ', num2str(chan2)])
+            
+             % Plot
+             figure;
+             plot(c1,polyval(z,c1),'r')
+             title('Model: polynomial regression (cubic)')
 
              % Plot fit at selections
              [r2_sel rmse_sel]=rsquare(c2(sel==1),fit(sel==1)); %rsquare() from file exchange
              %r=polyval(z,c1s); 
              figure;            
              scatter(fit(sel==1),c2(sel==1));
+             axis tight
+             axis normal
+             hold on
+             plot(c1,polyval(z,c1),'r')
              title(sprintf('Selections only: R^2 = %.3f, RMSE = %.3f',r2_sel,rmse_sel))
              xlabel('fit')
              ylabel('TargetEyePosition')
-             axis equal
+             %axis equal
              
              % Plot overall fit
              [r2 rmse]=rsquare(c2,fit); %rsquare() from file exchange
              figure;            
              scatter(fit,c2);
+             axis tight
+             axis normal
+             hold on
+             plot(c1,polyval(z,c1),'r')
              title(sprintf('All data: R^2 = %.3f, RMSE = %.3f',r2,rmse))
              xlabel('fit')
              ylabel('TargetEyePosition')
-             axis equal
+             %axis equal
+
             
         else
             disp('Channel to calibrate to does not exist in workspace');
